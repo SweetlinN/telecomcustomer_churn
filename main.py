2de42b68fffc2a5a9a6138b0da251daef357c261
nivetha <sweetlinN.sweetlinam621@gmail.com>
2026-09-16 09:10:01 +0530

    main.py file added

--- main.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-data = pd.read_csv('Data/Raw/telcom_customer_churn.csv.csv')
-print(data.head())
---
+++ src/main.py
@@ -0,0 +1,3 @@
+import pandas as pd
+data = pd.read_csv('../Data/Raw/telcom_customer_churn.csv.csv')
+print(data.head())
