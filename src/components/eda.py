79062b31c1906b12ebb434e82db263e1803b67a4
nivetha <sweetlinN.sweetlinam621@gmail.com>
2026-09-19 17:38:19 +0530

    Saved EDA results

--- src/components/eda.py
+++ src/components/eda.py
@@ -1,8 +1,13 @@
 import pandas as pd
+import os
+
+
+Artifacts_path = "../artifacts/reports"
+os.makedirs(Artifacts_path, exist_ok=True)
 
 
 def basic_eda(data:pd.DataFrame):
-    print('\n''+''='*60)
+    print('\n' + '=' * 60)
     print('Exploratory Data Analysis'.center(60))
     print('='*60)
 
@@ -18,6 +23,27 @@ def basic_eda(data:pd.DataFrame):
     print(data['gender'].value_counts())
 
     print(pd.crosstab(data['gender'], data['Churn']))
+
+    with open('dataset_summary.txt', 'w') as file:
+        print('started')
+        file.write('\n' + '=' * 60)
+
+
+
+        file.write('\nExploratory Data Analysis\n'.center(60))
+        file.write('\n' + '=' * 60)
+        print('mid')
+
+        file.write(f'\nDataset Shape: {data.shape}')
+
+        file.write('\nTarget Variable Distribution\n')
+        file.write(str(data['Churn'].value_counts()))
+
+
+        file.write('\nVariable Distribution\n')
+        file.write(str(data['gender'].value_counts()))
+
+
 
 
 def main():
